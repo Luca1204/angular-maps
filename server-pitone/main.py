f024680d894b33285c2b57d141ac20a29de6d614
Luca1204 <lombardi.luca.remo@iisgalvanimi.edu.it>
2021-11-10 12:54:35 +0000

    es 4 parte 2 quasi finito

--- server-pitone/main.py
+++ server-pitone/main.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app)
 CORS(app)
 
 # Annotation that allows the function to be hit at the specific URL.
-@app.route("/pippo")
+@app.route("/")
 # Generic Python functino that returns "Hello world!"
 def index():
     return "Hello world!"
@@ -24,11 +24,33 @@ def index():
 def get_all_addresses():
     mil4326WKT = mongo.db.MilWKT4326
     output = []
+    
     for s in mil4326WKT.find().limit(100):
-        output.append(s['INDIRIZZO'])
+        output.append(s['INDIRIZZO']+ "| "+ s["CI_VETTORE"])
+        
     return jsonify({'result': output})
+
+@app.route('/ci_vettore', methods=['GET'])
+def get_vettore():
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    for s in mil4326WKT.find().limit(100):
+        output.append(s['CI_VETTORE'])
+    return jsonify({'result': output})
+
+@app.route('/ci_vettore/<foglio>', methods=['GET'])
+def get_vettore1(foglio):
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    query = {
+        "FOGLIO" : foglio
+    }
+    for s in mil4326WKT.find(query).limit(100):
+        output.append(s['CI_VETTORE'])
+    return jsonify({'result': output})
+
 
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
     # Runs the Flask application only if the main.py file is being run.
-    app.run()
+        app.run()
